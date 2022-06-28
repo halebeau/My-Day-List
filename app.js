e373a07769e4c358f0c1941286c389ab7f954b21
Beau <halebeau3@gmail.com>
2022-06-28 18:19:44 -0500

    Added: Remove and css button to listItems

--- app.js
+++ app.js
@@ -2,35 +2,44 @@ const btnCreate = document.querySelector('.btn-main')
 const btnToggle = document.querySelector('.btn-toggle')
 const btnRemove = document.querySelector('.btn-remove')
 const taskList = document.querySelector('.list-container ul')
-const listItems = document.getElementsByTagName('li')
+const listItems = taskList.children
 
-for(let i=0; i < listItems.length; i++) {
-  listItems[i].addEventListener('mouseover', () => {
-    listItems[i].textContent = listItems[i].textContent.toUpperCase()
-  })
+
+function attachRemoveButton(li) {
+  let remove = document.createElement('button')
+  remove.className = 'remove'
+  remove.textContent = "Remove"
+  li.appendChild(remove)
+}
+for( let i = 0; i < listItems.length; i++ ) {
+  attachRemoveButton(listItems[i])
 }
 
-btnCreate.addEventListener("click", () => {
-  const input = document.querySelector(".input-main")
-  const list = document.querySelector('ul')
-
-  list.insertAdjacentHTML(
-    'afterbegin',
-    `<li>${input.value}</li>`
-  )
-  input.value = ""
-})
+  // taskList.addEventListener('mouseover', (event) => {
+  //   if( event.target.tagName === 'LI' ) {
+  //     event.target.textContent = event.target.textContent.toUpperCase()      
+  //   }
+  // })
 
 btnToggle.addEventListener("click", () => {
-  const listContainer = document.querySelector('.list-container')
-
-  if (listContainer.style.display === 'none') {
-    btnToggle.textContent = 'Hide List'
-    listContainer.removeAttribute('style')
+  const listContainer = document.querySelector(".list-container")
+  if (listContainer.style.display === "none") {
+    btnToggle.textContent = "Hide List"
+    listContainer.removeAttribute("style")
   } else {
     btnToggle.textContent = "Show List"
-    listContainer.style.display = 'none'
+    listContainer.style.display = "none"
   }
+})
+
+btnCreate.addEventListener("click", () => {
+  let ul = document.getElementsByTagName("ul")[0]
+  const input = document.querySelector(".input-main")
+  let li = document.createElement("li")
+  li.textContent = input.value
+  attachRemoveButton(li)
+  ul.appendChild(li)
+  input.value = ""
 })
 
 btnRemove.addEventListener('click', () => {
